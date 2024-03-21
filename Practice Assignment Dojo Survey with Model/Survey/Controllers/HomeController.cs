79999654737863e88c3c382a57360b72352d32a9
HammamiHichem <hichemhammami76@gmail.com>
2024-03-21 23:25:30 +0100

    Mise à jour des répertoires

---
+++ Practice Assignment Dojo Survey with Model/Survey/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using DojoSurveyWithModel.Models;
+
+namespace DojoSurveyWithModel.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult ProcessForm(DojoSurveyWithModel.Models.Survey result)
+        {
+            // Passing result as view model
+            return View("Result", result);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Practice Assignment Dojo Survey with Model/Survey/Models/Survey.cs
@@ -0,0 +1,10 @@
+namespace DojoSurveyWithModel.Models
+{
+    public class Survey
+    {
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public string? Language { get; set; }
+        public string? Comment { get; set; }
+    }
+}
